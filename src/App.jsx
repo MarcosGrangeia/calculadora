07c3b56b167678886672e390591126658beb603d
Marcosgrangeia <marcos.grangeia2@gmail.com>
2024-11-08 15:25:54 -0300

    calculadora

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return(
+    <main>
+      <h1>calculadora</h1>
+      <input type="number" placeholder="insira um número"/>
+      <input type="number" placeholder="insira um número"/>
+      <div>
+        <button>+</button>
+        <button>-</button>
+        <button>x</button>
+        <button>/</button>
+      </div>
+    </main>
+  )
+}
