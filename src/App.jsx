53befd830f25c9853d74aa45362dc2be52730eaf
Marcosgrangeia <marcos.grangeia2@gmail.com>
2024-11-13 15:50:08 -0300

    logica

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from "react"
 
 import "./style.scss"
 export default function App() {
-    const [primeiroValor, setSegundoValor] = useState()
+    const [primeiroValor, setPrimeiroValor] = useState()
+    const [segundoValor, setSegundoValor] = useState()
+    const capturandoPrimeiroValor =(e) => {
+      setPrimeiroValor(Number(e.target.value))
+    }
+    const capturandoSegundoValor =() => {
+      setSegundoValor(Number(e.target.value))
+    }
+
   return(
     <main>
       <h1>calculadora</h1>
-      <input type="number" placeholder="insira um número"/>
-      <input type="number" placeholder="insira um número"/>
+      <input onChange={capturandoPrimeiroValor} type="number" placeholder="insira um número"/>
+      <input onChange={capturandoSegundoValor} type="number" placeholder="insira um número"/>
       <div>
         <button>+</button>
         <button>-</button>
